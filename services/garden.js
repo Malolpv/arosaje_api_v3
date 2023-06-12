e98076b93428df61b2b8e5a0295febdb33b5cfcf
Malolpv <malo.lepavec@gmail.com>
2023-06-12 21:58:57 +0200

    add garden service

---
+++ services/garden.js
@@ -0,0 +1,21 @@
+const {Garden} = require('../sequelize/models')
+
+exports.findAllGardens = async () => {
+    return await Garden.findAll()
+}
+
+exports.findGardenById = async (id_garden) => {
+    return await Garden.findOne({where: {id: id_garden}})
+}
+
+exports.createGarden = async (garden) => {
+    return await Garden.create(garden)
+}
+
+exports.updateGarden = async (id_garden, garden) => {
+    return await Garden.update(garden,{where: {id: id_garden}})
+}
+
+exports.deleteGardenById = async (id_garden) => {
+    return await Garden.destroy({where: {id: id_garden}})
+}
