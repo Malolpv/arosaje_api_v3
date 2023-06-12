9da5b56dcf7e7a30fda997dbcc4fca9c5474ef3c
Malolpv <malo.lepavec@gmail.com>
2023-06-12 21:59:19 +0200

    add mission service

---
+++ services/mission.js
@@ -0,0 +1,22 @@
+const {Mission} = require('../sequelize/models')
+const mission = require('../sequelize/models/mission')
+
+exports.findAllMissions = async () => {
+    return await Mission.findAll()
+}
+
+exports.findMissionById = async (id_mission) => {
+    return await Mission.findOne({where: {id: id_mission}})
+}
+
+exports.createMission = async (mission) => {
+    return await Mission.create(mission)
+}
+
+exports.updateMission = async (id_mission, mission) => {
+    return await Mission.update(mission,{where: {id: id_mission}})
+}
+
+exports.deleteMissionById = async (id_mission) => {
+    return await Mission.destroy({where: {id: id_mission}})
+}
