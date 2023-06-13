0cc869e249c213eaf46a529ca9e412e19a1292a1
Malolpv <malo.lepavec@gmail.com>
2023-06-13 23:52:31 +0200

    added auth methods

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require("./jwtUtils");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = verifyToken(token);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(403).json({ error: "INVALID_TOKEN" });
+    }
+  } else {
+    res.status(401).json({ error: "MISSING_TOKEN" });
+  }
+}
+
+module.exports = authMiddleware;
