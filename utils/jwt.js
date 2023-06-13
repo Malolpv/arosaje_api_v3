265ee420d44b25fec9a888f5133ebe8ccd9147d3
Malolpv <malo.lepavec@gmail.com>
2023-06-13 23:52:18 +0200

    added jwt utils methods

---
+++ utils/jwt.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const jwt_secret = process.env.JWT_SECRET
+
+function generateToken(user) {
+  const payload = {
+    id: user.id,
+    email: user.email,
+    is_botaniste: user.is_botaniste
+    // Ajoutez d'autres informations que vous souhaitez inclure dans le token
+  };
+
+  return jwt.sign(payload, jwt_secret, { expiresIn: "1h" });
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, jwt_secret);
+}
+
+module.exports = {
+  generateToken,
+  verifyToken
+};
