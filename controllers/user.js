a2d663b7d8c76a92099f80f5047021f9d850ff9e
Malolpv <malo.lepavec@gmail.com>
2023-06-14 10:20:32 +0200

    htpp status refacto and user check on CRUD

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,14 @@
 const {findAllUsers, findUserById, deleteUserById, updateUser, isEmailTaken, createUser} = require('../services/user')
 const {findAllGardensByUserId, createGarden} = require('../services/garden')
+const { FORBIDDEN, NOT_FOUND, OK, INTERNAL_SERVER_ERROR } = require('../utils/http_status')
 
 exports.getUsers = async (req,res) => {
     try {
         const users = await findAllUsers()
-        if(users == null) return res.status(404).send('Not found')
-        res.status(200).json(users)
+        if(users == null) return res.status(NOT_FOUND).send('Not found')
+        res.status(OK).json(users)
     }catch(error){
-        res.status(500).send(error)
+        res.status(INTERNAL_SERVER_ERROR).send(error)
     }
 }
 
@@ -15,10 +16,10 @@ exports.getUser = async (req,res) => {
     try {
         const id = req.params.id
         const user = await findUserById(id)
-        if(user == null) return res.status(404).send("Not found")
-        res.status(200).json(user)
+        if(user == null) return res.status(NOT_FOUND).send("Not found")
+        res.status(OK).json(user)
     }catch(error){
-        res.status(500).send(error)
+        res.status(INTERNAL_SERVER_ERROR).send(error)
     }
     
 }
@@ -27,12 +28,12 @@ exports.getUserGardens = async (req,res) => {
     try {
         const user_id = req.params.id
         const gardens = await findAllGardensByUserId(user_id)
-        if(gardens == null) return res.status(404).send('Not found')
+        if(gardens == null) return res.status(NOT_FOUND).send('Not found')
 
-        res.status(200).json(gardens)
+        res.status(OK).json(gardens)
     }catch(error){
         console.error(error)
-        res.status(500).send(error)
+        res.status(INTERNAL_SERVER_ERROR).send(error)
     }
 }
 
@@ -40,13 +41,17 @@ exports.postUserGarden = async (req,res) => {
     try {
         const user_id  = req.params.id // Id du user propriétaire du jardin
         const { name, latitude, longitude, picture } = req.body // charge les données du jardin
-        
+        const logged_user_id = req.user.id
+
+        //on vérifie que c'est bien le user loggé qui essaye de s'ajouter un jardin
+        if (user_id != logged_user_id) return res.status('FORBIDDEN').json({error: 'FORBIDDEN'})
+
         // Recherchez l'utilisateur dans la base de données
         const user = await findUserById(user_id)
     
         if (!user) {
-          // Si l'utilisateur n'est pas trouvé, renvoie un code d'erreur 404 (Non trouvé)
-          return res.status(404).json('Not found')
+          // Si l'utilisateur n'est pas trouvé, renvoie un code d'erreur NOT_FOUND (Non trouvé)
+          return res.status(NOT_FOUND).json({error: 'Not found'})
         }
     
         // Créée une nouvelle instance de Garden avec les données reçues
@@ -57,8 +62,8 @@ exports.postUserGarden = async (req,res) => {
 
     } catch (error) {
         console.error(error)
-        // En cas d'erreur, répondez avec un code d'erreur approprié (par exemple, 500 pour une erreur interne du serveur) et un message d'erreur
-        res.status(500).send('Une erreur est survenue lors de la création du jardin.' )
+        // En cas d'erreur, répondez avec un code d'erreur approprié (par exemple, INTERNAL_SERVER_ERROR pour une erreur interne du serveur) et un message d'erreur
+        res.status(INTERNAL_SERVER_ERROR).send('Une erreur est survenue lors de la création du jardin.' )
     }
 }
 
@@ -66,13 +71,17 @@ exports.patchUser = async (req,res) => {
     try {
         const user_id = req.params.id // Id du user a update
         const { pseudo, email, password, is_botaniste } = req.body // Données a mettre à jour
-    
+        const logged_user_id = req.user.id
+
+        //On check que c'est bien le user lui meme qui veut mettre à jour son compte
+        if(user_id != logged_user_id) res.status(FORBIDDEN).json({error: 'Seul le propriétaire du compte peut mettre à jour ses données'})
+
         // Recherche l'utilisateur dans la base de données
         const user = await findUserById(user_id)
     
         if (!user) {
-          // Si l'utilisateur n'est pas trouvé, renvoie un code d'erreur 404 (Non trouvé)
-          return res.status(404).json('Not found')
+          // Si l'utilisateur n'est pas trouvé, renvoie un code d'erreur NOT_FOUND (Non trouvé)
+          return res.status(NOT_FOUND).json('Not found')
         }
     
         // Met à jour les propriétés de l'utilisateur
@@ -86,20 +95,25 @@ exports.patchUser = async (req,res) => {
 
     
         // Répondez avec l'utilisateur mis à jour
-        res.status(200).json(user)
+        res.status(OK).json(user)
     } catch (error) {
-        // En cas d'erreur, répondez avec un code d'erreur approprié (par exemple, 500 pour une erreur interne du serveur) et un message d'erreur
-        res.status(500).send('Une erreur est survenue lors de la mise à jour de l\'utilisateur.' )
+        // En cas d'erreur, répondez avec un code d'erreur approprié (par exemple, INTERNAL_SERVER_ERROR pour une erreur interne du serveur) et un message d'erreur
+        res.status(INTERNAL_SERVER_ERROR).send('Une erreur est survenue lors de la mise à jour de l\'utilisateur.' )
     }
 }
 
 exports.deleteUser = async (req,res) => {
     try{
         const id_user = req.params.id
+        const logged_user_id = req.user.id
+
+        //On check que c'est bien le user lui meme qui veut supprimer son compte
+        if(user_id != logged_user_id) res.status(FORBIDDEN).json({error: 'seul le propriétaire du compte peut supprimer son compte'})
+
         const nb_rows = await deleteUserById(id_user)
-        if(nb_rows <= 0) res.status(404).send("Not found")
-        res.status(200).send()
+        if(nb_rows <= 0) res.status(NOT_FOUND).send("Not found")
+        res.status(OK).send()
     }catch(error){
-        res.status(500).send(error)
+        res.status(INTERNAL_SERVER_ERROR).send(error)
     }
 }
