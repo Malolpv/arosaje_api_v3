26b9a712c4b1c2708195de88ca46f99a577f8652
Malolpv <malo.lepavec@gmail.com>
2023-06-17 19:19:16 +0200

    coorection returned messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ const register = async (req, res) => {
       // Check if the email already exists
       const existingUser = await emailExist(email)
       if (existingUser) {
-        return res.status(409).json({error: "An account already exist for this email"})
+        return res.status(409).json({message: "Un compte existe déjà pour cet email"})
       }
   
       // Hash the password
@@ -18,10 +18,10 @@ const register = async (req, res) => {
       // Create a new user
       const user = await createUser({pseudo: pseudo, email: email, password: hashedPassword, is_botaniste: is_botaniste})
       
-      res.status(201).json(user)
+      res.status(201).json({message: 'Utilisateur créé'})
     } catch (error) {
       console.error(error)
-      res.status(500).json({ error: "Internal server error" })
+      res.status(500).json({ message: "Erreur interne du serveur" })
     }
 }
 
@@ -32,13 +32,13 @@ const login = async (req, res) => {
       // Check if the email exists
       const user = await emailExist(email);
       if (!user) {
-        return res.status(401).json({ error: "Invalid username or password" })
+        return res.status(401).json({ message: "Email ou mot de passe invalide" })
       }
   
       // Compare the password
       const isPasswordValid = await bcrypt.compare(password, user.password)
       if (!isPasswordValid) {
-        return res.status(401).json({ error: "Invalid username or password" })
+        return res.status(401).json({ message: "Email ou mot de passe invalide" })
       }
   
       // Generate JWT token
@@ -47,7 +47,7 @@ const login = async (req, res) => {
       res.status(200).json({ token });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Internal server error" });
+      res.status(500).json({ message: "Internal server error" });
     }
 }
 
