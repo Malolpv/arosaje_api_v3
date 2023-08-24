3d71bad51ed10b96d21babf188a7aed6ca659460
Malolpv <malo.lepavec@gmail.com>
2023-08-24 15:32:45 +0200

    docstrings

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,16 @@
+const { UNAUTHORIZED, FORBIDDEN } = require('../utils/http_status')
 const { verifyToken } = require('../utils/jwt') 
+require('../utils/http_status')
 
+/**
+ * Check if the token contained in request Authorization header is valid.
+ * If the token is missing, set the response to 401.
+ * If the token is invalid, set the response to 403.
+ * Else execute the callback function.
+ * @param {*} req The handled htpp request
+ * @param {*} res Server response
+ * @param {*} next Callback function
+ */
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization 
     
@@ -12,10 +23,10 @@ const authMiddleware = (req, res, next) => {
         next() 
         } catch (error) {
             console.error(error)
-            res.status(403).json({ error: "INVALID_TOKEN" }) 
+            res.status(FORBIDDEN).json({ error: "INVALID_TOKEN" }) 
         }
     } else {
-    res.status(401).json({ error: "MISSING_TOKEN" }) 
+    res.status(UNAUTHORIZED).json({ error: "MISSING_TOKEN" }) 
     }
 }
 
